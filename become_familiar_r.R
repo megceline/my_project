4f6ca66f32aae420c723c965b2c034bacd22f264
meg hernandez <meghernandez@megs-MacBook-Pro.local>
2020-01-31 23:38:11 -0800

    become_familiar_r.R

---
+++ become_familiar_r.R
@@ -0,0 +1,89 @@
+# Where I am? 
+getwd()
+# I want to settle down where I like. 
+setwd("/Users/meghernandez/HS611")
+# What is inside my home?
+list.files()
+# Say hello to the world but don't forget my cat!
+print("hello world")
+cat('my cat says mew', 5+7)
+# I don't care that hash sign.
+#####
+# Help me, guys! 
+help.start()
+# My strength just after birth 
+search()
+# Adding some strength 
+install.packages("rio")
+# Check my strength
+installed.packages()
+# Show of my strength
+library(rio)
+# Show off time is over!
+detach(package.rio)
+# You can quit me with a single word.
+q()
+# Give me command one at a time but if you want me to give
+
+# multiple commands at a time don't forget the partition.
+a <- 5; b <- 6
+
+
+# Did you like history in high school? Then it's for you.
+history()
+
+
+
+# Assignment?
+a <- 5
+b <- 6
+
+
+# list the assignments
+ls()
+
+
+# Remove assignment
+rm(a)
+
+# Remove all assignments
+rm(list = ls())
+
+
+# What gift is within the package?
+data(package="datasets")
+
+
+# Scalar data types (holds a single data item), check it.
+
+x <- 2.5
+b  <- 7
+ls.integer(x)
+class(x)
+class(b)
+# Things change (variable conversion)
+as.integer(x)
+# Vector
+
+y <- c(1, 2, 3, 4, 5)
+1:5
+seq(c(1:20, length=40))
+seq_along(c(5, 6, 7, 8))
+
+rep(4, 7)
+rep(c(1, 2, 3, 4),each=4)
+replicate(2, 3)
+replicate(2, c(1, 2, 3))
+
+y[-4]
+
+y[ , 5]
+
+max[y]
+
+#
+
+
+
+
+
